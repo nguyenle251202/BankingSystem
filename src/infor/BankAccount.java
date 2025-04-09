a9a20c517777660abed4ca28a1d22cfa827d085b
nguyenle251202 <120208210+nguyenle251202@users.noreply.github.com>
2025-04-09 15:13:43 +0700

    151209042025-BankAccount

---
+++ src/infor/BankAccount.java
@@ -0,0 +1,49 @@
+package infor;
+
+public class BankAccount {
+    private String accountNumber;
+    private String ownerName;
+    private double balance;
+
+    public BankAccount(String accountNumber, String ownerName, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.balance = initialBalance;
+    }
+
+    // Getters
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    //Setters
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    //ToStrings
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", ownerName='" + ownerName + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
